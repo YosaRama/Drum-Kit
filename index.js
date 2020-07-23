cd28c897e0435b318c2b76566f6f229e63cc2fc2
YosaRama <67961793+YosaRama@users.noreply.github.com>
2020-07-23 18:09:26 +0800

    Drum Kit

---
+++ index.js
@@ -0,0 +1,65 @@
+// CHEKING DRUM INNER INPUT
+
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButton; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var clciksound = this.innerHTML;
+        drumSound(clciksound);
+        buttonAnimation(clciksound);
+    });
+}
+
+
+// CHECKINNG USER INPUT
+
+document.addEventListener("keydown", function(event){
+    drumSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function drumSound(key) {
+    switch (key) {
+        case "w":
+            var kickDrum = new Audio("sounds/kick-bass.mp3");
+            kickDrum.play();
+            break;
+        case "a":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "d":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default: console.log(DrumButton);
+            break;
+    }
+}
+
+function buttonAnimation(currentkey){
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    })
+}
